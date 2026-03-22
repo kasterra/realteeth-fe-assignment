89dc53ecb087cbffbb5e16a47eb6f527061f34e1
kasterra <kasterra@naver.com>
2026-03-22 19:10:38 +0900

    feat: tanstack query queryclient 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,10 @@
+import { queryClient } from "@/shared/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </QueryClientProvider>
+  );
 }
